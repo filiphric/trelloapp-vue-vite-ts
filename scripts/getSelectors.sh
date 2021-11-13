0ef95d186ff6fe10abe19f4a0eef1491617647a9
Filip Hric <filip@filiphric.com>
2021-11-13 23:40:48 +0100

    improve getSelectors script

--- scripts/getSelectors.sh
+++ scripts/getSelectors.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 #         find all data-cy attribute in src | select values |
-SELECTORS=$(grep -hro 'data-cy="[^"]*"' src | cut -d \" -f2 | sort | uniq)
+SRC_SELECTORS=$(grep -hro 'data-cy="[^"]*"' src | cut -d \" -f2 | sort | uniq)
+TEST_SELECTORS=$(grep -hro '.getDataCy([^"]*' cypress | cut -d \' -f2 | sort | uniq)
 
-#                remove all spaces ;       add string to the start      ; add to the end |            write to a file 
-echo $SELECTORS | sed "s/ /'\n| '/g; s/^/&export type Selectors = \n| '/; s/.$/&'/;" | cat > cypress/support/@types/selectors.d.ts
+UNUSED_SELECTORS=$(comm -23 <(echo "$SRC_SELECTORS") <(echo "$TEST_SELECTORS"))
+
+if [[ $UNUSED_SELECTORS != "" ]]; then
+echo -e "WARNING! There are some selectors in your app that are not being used:
+
+$UNUSED_SELECTORS"
+fi
+
+echo $SRC_SELECTORS | sed "s/ /'\n| '/g; s/^/&export type Selectors = \n| '/; s/.$/&'/;" | cat > cypress/support/@types/selectors.d.ts
