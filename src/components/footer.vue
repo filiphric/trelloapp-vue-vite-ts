8c5707ddc2b7c8c9deaa28ac21638e9706e574d1
Filip Hric <filip@filiphric.com>
2022-02-16 22:38:45 +0100

    add whitespaces for emoji

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -4,7 +4,7 @@
     class="absolute bottom-2 w-full text-xs text-center text-blue10 opacity-75"
     href="https://filiphric.com"
   >
-    ...powered by coffee and love ❤️ Filip Hric
+    ...powered by coffee and love ❤️&nbsp;&nbsp;Filip Hric
   </a>
 </template>
 
