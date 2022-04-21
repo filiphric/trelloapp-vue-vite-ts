6e9bebe3cb08962c49113f46eb0d12d3e89d6653
Filip Hric <filip@filiphric.com>
2022-04-21 16:31:23 +0200

    add clean version
    fix caching problem

--- src/components/Footer.spec.ts
+++
@@ -1,7 +0,0 @@
-import Footer from '@/components/Footer.vue';
-
-it('shows footer', () => {
-  cy.mount(Footer);
-
-  cy.getDataCy('footer-link').should('be.visible').and('contain.text', '❤️');
-});
