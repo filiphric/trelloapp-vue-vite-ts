8db3b4c7e8d16b0ddca681a028b8d80a27537375
Filip Hric <filip@filiphric.com>
2022-06-11 20:45:25 +0200

    remove footer component

--- src/components/footer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <a
-    data-cy="footer-link"
-    class="absolute bottom-2 w-full text-xs text-center text-blue10 opacity-75"
-    href="https://filiphric.com"
-  >
-    ...powered by coffee and love ❤️&nbsp;&nbsp;Filip Hric
-  </a>
-</template>
