ae4f43a3a68deb7a39e6a6979884d1aedcedb834
Filip Hric <filip@filiphric.com>
2022-09-20 22:17:58 +0200

    fix card validations

--- backend/api/card-routes.ts
+++ backend/api/card-routes.ts
@@ -5,26 +5,28 @@ const moment = require('moment');
 const jsonServer = require('json-server');
 const app = jsonServer.create();
 
-app.post('/', ({ body }, res, next) => { 
-  
-validate(['boardId', 'listId', 'name'], body, res)
-  
-  // data generation
-  body.created = moment().format('YYYY-MM-DD');
-  body.deadline = moment()
+app.post('/', ({ body }, res, next) => {
+
+  validate(['boardId', 'listId', 'name'], body, res)
+
+  if (res.statusCode !== 400) {
+    // data generation
+    body.created = moment().format('YYYY-MM-DD');
+    body.deadline = moment()
       .add(3, 'days')
       .format('YYYY-MM-DD'),
-  body.description = '';
-  body.completed = false;
+      body.description = '';
+    body.completed = false;
 
-  // stream message
-  //  socket.emit('cardCreated', body.listId, body);
+    // stream message
+    //  socket.emit('cardCreated', body.listId, body);
 
-  next()
+    next()
 
+  }
 });
 
-app.delete('/', ({ app: { parent: { db } } }, res) => { 
+app.delete('/', ({ app: { parent: { db } } }, res) => {
 
   db.set('cards', []).write();
 
