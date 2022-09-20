6c18264a9b36ceca463a5c0dba204f1b5e910c61
Filip Hric <filip@filiphric.com>
2022-09-20 16:12:41 +0200

    add validation for board body

--- backend/api/board-routes.ts
+++ backend/api/board-routes.ts
@@ -2,8 +2,9 @@ const jsonServer = require('json-server');
 const app = jsonServer.create();
 const moment = require('moment');
 import { getUserId } from '../utils/getUserId'
+import { validate } from '../utils/validate';
 
-app.get('/', ({ app: { parent: { db } }, headers, query }, res) => { 
+app.get('/', ({ app: { parent: { db } }, headers, query }, res) => {
 
   if (query?.starred === 'true') {
     query.starred = true
@@ -31,23 +32,25 @@ app.get('/', ({ app: { parent: { db } }, headers, query }, res) => {
 
 app.post('/', ({ headers, body }, res, next) => {
 
+  validate(['body'], body, res)
+
   body.user = getUserId(headers) || 0;
   body.starred = false;
   body.created = moment().format('YYYY-MM-DD');
-  
+
   // socket.emit('boardCreated', req.body);
 
   next()
 
 })
 
-app.delete('/', ({ app: { parent: { db } } }, res) => { 
+app.delete('/', ({ app: { parent: { db } } }, res) => {
 
-    db.set('boards', []).write();
-    db.set('lists', []).write();
-    db.set('cards', []).write();
+  db.set('boards', []).write();
+  db.set('lists', []).write();
+  db.set('cards', []).write();
 
-    return res.status(204).end();
+  return res.status(204).end();
 
 })
 
